81a6da0250d174621f499eec298929dd0b04535f
Tim Dettinger <6506703@isd535.org>
2022-03-22 10:36:28 -0500

    added create and deliver order

--- scripts/truck.js
+++ scripts/truck.js
@@ -3,7 +3,19 @@
 
     let App = window.App || {};
 
-    function Trick() {
+    function Truck(truckId, db) {
+        this.truckId = trickId;
+        this.db = db;
+    }
+
+    Truck.prototype.createOrder = function (order) {
+        console.log('Adding order for ' + order.emailAdress);
+        this.db.add(order.emailAdress, order);
+    }
+
+    Truck.prototype.deliverOrder = function (customerId) {
+        console.log('delivering order for ' + customerId);
+        this.db.remove(customerId);
     }
 
     App.Truck = Truck;
