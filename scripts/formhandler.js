358d2eec59548ad7d67ee38d78aaccead4b01af6
Tim Dettinger <6506703@isd535.org>
2022-03-23 11:27:32 -0500

    create formhandler and add to html

---
+++ scripts/formhandler.js
@@ -0,0 +1,12 @@
+(function (window) {
+    'use strict';
+
+    let App = window.App || {};
+
+    function FormHandler() {
+        // do stuff here
+    }
+
+    App.FormHandler = FormHandler;
+    window.App = App;
+})(window);
