2aaab80c232ba4a2d7d4a8771da5fc9bd4467168
Tim Dettinger <6506703@isd535.org>
2022-04-04 11:16:25 -0500

    adding submit handler

--- scripts/formhandler.js
+++ scripts/formhandler.js
@@ -4,10 +4,27 @@
     let App = window.App || {};
     let $ = window.jQuery;
 
-    function FormHandler() {
+    function FormHandler(selector) {
+        //do stuff
         if (!selector) {
             throw new Error('No selector Provided!');
         }
+
+
+        this.$formElement = $(selector);
+        if (this.$formElement.length == 0) {
+            throw new Error('Could not find element with selector: ' + selector);
+        }
+    }
+
+    FormHandler.prototype.addSubmitHandler = function () {
+        console.log('Setting the submit hander for the form...');
+        this.$formElement.on('submit', function(event) {
+            event.preventDefault();
+
+            let data = $(this).serializeArray();
+            console.log(data);
+        });
     }
 
     App.FormHandler = FormHandler;
