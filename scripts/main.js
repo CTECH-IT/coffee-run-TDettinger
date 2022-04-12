627fae2407a5ac6fc08207a4f70d73513e11931e
Tim Dettinger <6506703@isd535.org>
2022-04-12 11:33:36 -0500

    unit 4

--- scripts/main.js
+++ scripts/main.js
@@ -2,17 +2,25 @@
     'use strict'; 
 
     const FORM_SELECTOR = '[data-coffee-order="form"]';
+    const CHECKLIST_SELECTOR ='[data-coffee-order="checklist"]';
+
     let App = window.App;
     let Truck = App.Truck;
     let DataStore = App.DataStore;
     let FormHandler = App.FormHandler;
+    let CheckList = App.CheckList
 
     let myTruck= new Truck('12345', new DataStore());
-
     window.mytruck = myTruck;
 
     let formHandler = new FormHandler(FORM_SELECTOR);
-    formHandler.addSubmitHandler(myTruck.createOrder.bind(myTruck));
-    console.log(formHandler);
+
+    let checkList = new CheckList(CHECKLIST_SELECTOR);
+
+
+    formHandler.addSubmitHandler(function (data) {
+        myTruck.createOrder.call(myTruck, data);
+        checkList.addRow.call(checkList, data);
+    });
 
 })(window);
