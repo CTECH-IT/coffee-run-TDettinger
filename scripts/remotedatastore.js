3cf1686189aa011e264008abb516d9b8c7826062
Tim Dettinger <6506703@isd535.org>
2022-04-15 11:04:24 -0500

    created remotedatastore

---
+++ scripts/remotedatastore.js
@@ -0,0 +1,29 @@
+(function (window) {
+    'use strict'
+
+    var App = window.App || {};
+    var $ = window.jQuery;
+    function RemoteDataStore(url) {
+        if (!url) {
+            throw new Error('No remote URL supplied.');
+        }
+        this.serverUrl = url;
+    };
+
+    RemoteDataStore.prototype.add = function (key, val) {
+        $.post(this.serverUrl, val, function (serverResponse) {
+            console.log(serverResponse);
+        });
+    };
+
+    RemoteDataStore.prototype.getAll = function () {
+        $.get(this.serverUrl, function (serverReponse) {
+            console.log(serverReponse);
+            cb(serverResponse);
+        });
+    };
+
+    App.RemoteDataStore = RemoteDataStore;
+    window.App = App;
+
+})(window)
